e02279e14a7ccbe0caf25c7d0774df9b762ad10e
zohra <zsohrabi15@gmail.com>
2026-04-22 10:56:55 +0100

    Update setup and requirements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 #system dependencies
+brew install python@3.11
 brew install glui
+brew install cddlib
+
+#create virtual environment and activate
+python3.11 -venv venv
+source venv/bin/activate
 
 #Python dependencies
 pip install -r requirements.txt
